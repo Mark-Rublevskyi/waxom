3d91c972c7861f32e5e14c335f94b7b06ccc109b
Mark Rublevskiyy <57556412+Mark-Rublevskiy@users.noreply.github.com>
2020-09-06 00:14:07 +0300

    site

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(".header-content").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    Infinity: true
+});
+
+$(".recent-posts-cards").slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
+    Infinity: true
+});
